c7197b50cd01697e32c2c1b9b8737a74bb28226c
magnus2021 <magnus@shipedge.com>
2021-06-11 23:29:47 -0400

    ref deleting mapper service

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,7 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
-import { MapperService } from '../../shared/mapper.service';
 import { UserDto } from './dto/user.dto';
 import { User } from './user.entity';
 import { UserDetails } from './user.details.entity';
@@ -12,10 +11,9 @@ import { Role } from '../role/role.entity';
 export class UserService {
     constructor(
         @InjectRepository(UserRepository)
-        private readonly _userRepository: UserRepository,
-        private readonly _mapperService: MapperService,
+        private readonly _userRepository: UserRepository
     ){}
-    async get(id: number): Promise<UserDto>{
+    async get(id: number): Promise<User>{
         if(!id){
             throw new BadRequestException('id must be sent')
         }
@@ -28,21 +26,18 @@ export class UserService {
             throw new NotFoundException()
         }
 
-        return this._mapperService.map<User, UserDto>(user, new UserDto())
+        return user
     }
 
-    async getAll(): Promise<UserDto[]>{
+    async getAll(): Promise<User[]>{
         const users: User[] = await this._userRepository.find({
             where: {status: 'ACTIVE'},
         })
 
-        return this._mapperService.mapCollection<User, UserDto>(
-            users, 
-            new UserDto()
-        )
+        return users
     }
 
-    async create(user: User): Promise<UserDto>{
+    async create(user: User): Promise<User>{
         const details = new UserDetails()
         user.details = details
 
@@ -51,7 +46,7 @@ export class UserService {
         user.roles = [defaultRole]
         
         const savedUser: User = await this._userRepository.save(user)
-        return this._mapperService.map<User, UserDto>(savedUser, new UserDto())
+        return savedUser
     }
 
     async update(id: number, user: User): Promise<void>{

8078949e27891df7042426e4a5fd8502b6f79826
magnus2021 <magnus@shipedge.com>
2021-06-11 21:25:41 -0400

    add: role controller, entity and repo

--- src/modules/role/role.module.ts
+++ src/modules/role/role.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RoleController } from './role.controller';
+import { RoleRepository } from './role.repository';
+import { RoleService } from './role.service';
+@Module({
+    imports: [TypeOrmModule.forFeature([RoleRepository])],
+    providers: [RoleService],
+    controllers: [RoleController],
+})
 export class RoleModule {}
